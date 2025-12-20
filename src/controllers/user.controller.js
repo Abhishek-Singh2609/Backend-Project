52e9c552560444f76c9a51584c64e44e69c8a7fb
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-21 01:21:20 +0530

    add login check point

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -136,7 +136,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const { email, username, password } = req.body;
   console.log(email);
 
-  if (!username && !email) {
+  if (!(username && email)) {
     throw new ApiError(400, "username or email is required");
   }
 
