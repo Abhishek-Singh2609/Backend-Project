d9d5a3b2ba11542839335956e05185fbfff2a531
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-27 00:10:33 +0530

    Add tweets route to app.js for tweeting

--- src/app.js
+++ src/app.js
@@ -30,5 +30,6 @@ import dashboardRouter from "./routes/dashboard.routes.js";
 // Router declaration
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
 
 export { app };
