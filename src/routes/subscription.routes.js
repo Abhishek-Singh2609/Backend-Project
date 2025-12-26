1cb3e5378674588da078dad5a49d7874cef4d476
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-26 23:36:14 +0530

    Add subscription routes with JWT verification middleware

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router
+  .route("/c/:channelId")
+  .get(verifyJWT, getSubscribedChannels)
+  .post(verifyJWT, toggleSubscription);
+
+router.route("/u/:subscriberId").get(verifyJWT, getUserChannelSubscribers);
+
+export default router;
