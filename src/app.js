61d6458454b8e87e861162f49ae5f38222d788c7
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-26 23:58:09 +0530

    Refactor app.js for improved readability and add additional route imports

--- src/app.js
+++ src/app.js
@@ -2,23 +2,29 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app= express();
+const app = express();
 
-app.use(cors({
-    origin: process.env.CORS_ORIGIN ,
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 // routes import
-
 import userRouter from "./routes/user.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+
 // Router declaration
+app.use("/api/v1/users", userRouter);
 
-app.use("/api/v1/users", userRouter)
-
-export{ app }
+export { app };
