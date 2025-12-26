7721e825097d447b7f5cf58920860c57086a98db
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-26 23:59:12 +0530

    Add like routes to app.js for handling user likes

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
+import likeRouter from "./routes/like.routes.js";
 
 // Router declaration
 app.use("/api/v1/users", userRouter);
