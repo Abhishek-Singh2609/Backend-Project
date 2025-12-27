1b18662170862beb2f5ece19d8fe992e65de5dc6
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-27 22:55:44 +0530

    Add subscription routes to app.js for managing user videos

--- src/app.js
+++ src/app.js
@@ -32,4 +32,6 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/tweets", tweetRouter);
 import subscriptionRouter from "./routes/subscription.routes.js";
+import videoRouter from "./routes/video.routes.js";
+
 export { app };
