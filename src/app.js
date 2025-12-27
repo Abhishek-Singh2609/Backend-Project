6b68d0d0c40590a0057965f16bfee45d91b213bd
Abhishek-Singh2609 <techabhisheksingh2609@gmail.com>
2025-12-27 23:05:57 +0530

    Add playlist & dashboard routes to app.js for the users

--- src/app.js
+++ src/app.js
@@ -35,5 +35,7 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
